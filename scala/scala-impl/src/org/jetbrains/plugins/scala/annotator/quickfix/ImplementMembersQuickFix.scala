ebb85a63d5c02ddd1e5309b0c8132a194bc068c7
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2022-07-14 16:01:26 +0200

    Enable Implement Members quick-fix when compiler-based highlighting is enabled #SCL-19751 Fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ImplementMembersQuickFix.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/quickfix/ImplementMembersQuickFix.scala
@@ -14,15 +14,12 @@ import org.jetbrains.plugins.scala.overrideImplement.ScalaOIUtil
  * Date: 22.09.2008
  */
 
-class ImplementMembersQuickFix(clazz: ScTemplateDefinition) extends IntentionAction {
-
+final class ImplementMembersQuickFix(clazz: ScTemplateDefinition) extends IntentionAction {
   override def getText: String = ScalaBundle.message("implement.members.fix")
   override def startInWriteAction: Boolean = false
 
   override def isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean =
-    if (!clazz.isValid) false
-    else if (!file.isWritable) false
-    else true
+    clazz.isValid && file.isWritable
 
   override def invoke(project: Project, editor: Editor, file: PsiFile): Unit =
     ScalaOIUtil.invokeOverrideImplement(file, isImplement = true)(project, editor)
